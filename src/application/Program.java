55df29738c1e849f34004bc730ca1273ac319233
Bruno Albarado <brunoalbarado19@gmail.com>
2022-10-06 12:56:36 -0300

    findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,26 +1,21 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		
-		Department obj = new Department(1,"Books");
-		
-		Seller seller  = new Seller(32,"Bruno Albarado de Andrade","brunoalbarado19@gmail.com" ,new Date() , 3.000, obj);
-		
+			
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
-
+		
+		
 	}
 
 }
